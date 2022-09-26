ec0db31770f2bfa08120e9ab1f74933b726ae5ca
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2022-09-27 01:08:59 +0530

    design fixes

--- src/components/SucessScreen/SucessScreen.js
+++ src/components/SucessScreen/SucessScreen.js
@@ -5,11 +5,11 @@ function SucessScreen() {
   const docEmail = localStorage.getItem("docEmail");
   return (
     <div className="bg-white p-5 h-screen flex flex-col items-center justify-evenly">
-      <h1 className="text-4xl leading-10 text-white font-regular">
+      <h1 className="text-4xl leading-10 text-gray-800 font-regular">
         Your prescription has been sent to your {docEmail}. Please check and
         sign.
       </h1>
-      <h2 className="text-3xl leading-10 text-white font-regular">Patient will receive a copy of the same on their email, after you sign.</h2>
+      <h2 className="text-3xl leading-10 text-gray-800 font-regular">Patient will receive a copy of the same on their email, after you sign.</h2>
       <img src="https://c.tenor.com/0AVbKGY_MxMAAAAM/check-mark-verified.gif" alt="tick" />
       <button
         class="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
