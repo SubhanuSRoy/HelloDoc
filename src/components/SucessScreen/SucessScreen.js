3a3acfebc3dcd7653d58c93dd868ee03e0d991f6
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2022-09-27 00:40:56 +0530

    removed phone number

--- src/components/SucessScreen/SucessScreen.js
+++ src/components/SucessScreen/SucessScreen.js
@@ -5,12 +5,12 @@ function SucessScreen() {
   const docEmail = localStorage.getItem("docEmail");
   return (
     <div className="bg-white p-5 h-screen flex flex-col items-center justify-evenly">
-      <h1>
+      <h1 className="text-4xl leading-10 text-white font-regular">
         Your prescription has been sent to your {docEmail}. Please check and
         sign.
       </h1>
-      <h2>Patient will receive a copy of the same on their email</h2>
-      <img src="https://c.tenor.com/0AVbKGY_MxMAAAAM/check-mark-verified.gif" />
+      <h2 className="text-3xl leading-10 text-white font-regular">Patient will receive a copy of the same on their email, after you sign.</h2>
+      <img src="https://c.tenor.com/0AVbKGY_MxMAAAAM/check-mark-verified.gif" alt="tick" />
       <button
         class="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
         type="submit"
